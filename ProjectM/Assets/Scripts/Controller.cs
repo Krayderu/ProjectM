ced157694e750ffc3476f08aa4abad87ac6fc4f7
Sébastien-Rieder <creeder200@gmail.com>
2024-01-25 00:09:21 +0100

    Player can move

--- ProjectM/Assets/Scripts/Controller.cs
+++ ProjectM/Assets/Scripts/Controller.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 mousePositionWorld;
+    [SerializeField] GameObject player;
+    private void Update()
     {
-        
+        MovePlayer();
+
+    }
+    public void GetMousePosition()
+    {
+        // Check for mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Get the mouse position in screen coordinates
+            Vector3 mousePositionScreen = Input.mousePosition;
+
+            // Use the main camera to convert screen position to world position
+            Camera mainCamera = Camera.main;
+            if (mainCamera != null)
+            {
+                mousePositionWorld = mainCamera.ScreenToWorldPoint(new Vector3(mousePositionScreen.x, mousePositionScreen.y, 0));
+
+                // Now you have the world position of the mouse click
+                
+                Debug.Log("Mouse Clicked at: " + mousePositionWorld);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MovePlayer()
     {
+        GetMousePosition();
+        var newPlayerPos = new Vector2(mousePositionWorld.x, player.transform.position.y);
+        player.transform.SetPositionAndRotation(newPlayerPos, player.transform.rotation);
         
     }
 }
